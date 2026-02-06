c47cda6e9aa8f822d1b59c02ed228cfade837dd5
Saidurgatalluri <125432477+Saidurgatalluri@users.noreply.github.com>
2026-02-06 12:42:34 +0530

    Fix typo in backend.tf for use_lockfile

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "saidurga.s3bucket.com" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
+    use_locakfile=true
   }
 }
