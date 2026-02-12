c96c9bc438e7708bf1c11d4b1f9c67d6b80ba93e
Saidurgatalluri <125432477+Saidurgatalluri@users.noreply.github.com>
2026-02-12 16:13:25 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "saidurga.s3bucket.com" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
-    use_lockfile=true
   }
 }
